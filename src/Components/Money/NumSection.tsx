7d2b97b46ca7078361bf09d6cfa3ce474df7dd4c
sbgrb <974487599@qq.com>
2020-09-10 23:44:15 +0800

    重构NumSection

--- src/Components/Money/NumSection.tsx
+++ src/Components/Money/NumSection.tsx
@@ -1,34 +1,50 @@
-import styled from "styled-components";
+import React, {useState} from "react";
+import {Wrapper} from "./NumSection/Wrapper";
+import {getOutput} from "./NumSection/getOutput";
 
-const NumSection = styled.section`
-   display:flex;
-   flex-direction: column;
-   font-size: 18px;
-   > .output{
-     font-size: 36px;
-     line-height: 72px;
-     text-align: right;
-     padding: 0 16px;
-     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                 inset 0 5px 5px -5px rgba(0,0,0,0.25);
-   }
-   > .pad{
-        > button{
-        float: left;
-        width: 25%;
-        border: none;
-        height: 64px;
-         &.ok{
-          height: 128px;
-          float: right;
-         }
-         &.zero{
-         width: 50%;
-         }
-         &:nth-child(1){
-         background: greenyellow;
-         }
-      }
-   }
-`
+
+const  NumSection: React.FC = () =>{
+    const [output,_setOutput] = useState('0')
+    const setOutput = (output : string) =>{
+        if (output.length > 16){
+            output = output.slice(0,16)
+        }else if (output.length === 0){
+            output = '0'
+        }
+        _setOutput(output)
+    }
+    const onClickButtonWrapper = (e: React.MouseEvent) =>{
+        const text =  (e.target as HTMLButtonElement).textContent
+        if (text === null){return}
+        if (text === 'ok'){
+            return;
+        }
+        if ('0123456789'.split('').concat(['删除','清空']).indexOf(text) >= 0){
+           setOutput(getOutput(text,output))
+        }
+    }
+    return(
+        <Wrapper>
+            <div className='output'>
+                {output}
+            </div>
+            <div className='pad clearfix' onClick={onClickButtonWrapper}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className='ok'>OK</button>
+                <button className='zero'>0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>
+    )
+}
 export {NumSection}
