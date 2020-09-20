8f591a51b876779078689f21f173e24560b775db
sbgrb <974487599@qq.com>
2020-09-21 00:05:17 +0800

    修改无法输入浮点数

--- src/Components/Money/NumSection.tsx
+++ src/Components/Money/NumSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Wrapper} from "./NumSection/Wrapper";
 import {getOutput} from "./NumSection/getOutput";
 
@@ -10,17 +10,19 @@ type Props = {
 }
 
 const NumSection: React.FC<Props> = (props) => {
-    const output = props.value.toString()
+    // const output = props.value.toString()
+    const [output, _setOutput] = useState(props.value.toString())
     const setOutput = (output: string) => {
-        let value
+        let newOutput: string;
         if (output.length > 16) {
-            value = parseFloat(output.slice(0, 16))
+            newOutput = output.slice(0, 16)
         } else if (output.length === 0) {
-            value = 0
+            newOutput = '0'
         }else {
-            value = parseFloat(output)
+            newOutput = output
         }
-        props.onChange(value)
+        _setOutput(newOutput)
+        props.onChange(parseFloat(newOutput))
     }
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent
@@ -31,7 +33,7 @@ const NumSection: React.FC<Props> = (props) => {
             if (props.onOk){props.onOk()}
             return;
         }
-        if ('0123456789'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+        if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
             setOutput(getOutput(text, output))
         }
     }
